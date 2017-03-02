0d1c77cd2d81e6c29eb137fcbf24045c65a11d12
marek.czapiewski1@gmail.com <marek.czapiewski1@gmail.com>
2017-03-02 21:10:12 +0100

    - init

---
+++ Wzmacniacz_program/main.c
@@ -0,0 +1,18 @@
+/*
+ * Wzmacniacz_program.c
+ *
+ * Created: 02.03.2017 21:08:25
+ * Author : Marek
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
